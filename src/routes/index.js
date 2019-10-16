c91f9dd248b2f9ff4d5f887bf2850aa5beb5fe2c
Emysilva <emysly12@gmail.com>
2019-10-16 03:58:20 +0100

    created the login function

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ import UserController from "../controllers/user.controller";
 const router = express.Router();
 
 router.post("/user/signup", UserController.create);
+router.post("/user/login", UserController.login);
 
 export default router;
